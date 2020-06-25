6248ffa3410f8916a8a9f53f01e7e9b7186fbb8d
Atilio Gabriello <gabrielloatilio@gmail.com>
2020-06-25 17:39:40 -0300

    Novas regras

--- JS/jogo.js
+++ JS/jogo.js
@@ -18,7 +18,8 @@ function novoJogo(){
     roundScore = 0;// Pontuação de cada round
     jogadorAtivo = 0;// 0 === jogador 1 / 1 === jogador 2
     jogoRolando = true;
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice-0').style.display = 'none';
+    document.querySelector('.dice-1').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
@@ -37,18 +38,34 @@ function novoJogo(){
 novoJogo();
 document.querySelector('.btn-roll').addEventListener('click', function(){
     if(jogoRolando){
-        var dado = Math.floor(Math.random() * 6) + 1;// Gera um número aleatório entre 1 e 6
-        var dadoDOM = document.querySelector('.dice');
-        dadoDOM.style.display = 'block';
-        dadoDOM.style.opacity = 1;
-        dadoDOM.src = 'Resources/dice-' + dado + '.png';
-        if(dado !== 1){
-            roundScore += dado;
-            document.querySelector('#current-' + jogadorAtivo).textContent = roundScore;
+        var dado_0 = Math.floor(Math.random() * 6) + 1;// Gera um número aleatório entre 1 e 6
+        var dado_1 = Math.floor(Math.random() * 6) + 1;// Gera um número aleatório entre 1 e 6
+        var dadoDOM_0 = document.querySelector('.dice-0');
+        var dadoDOM_1 = document.querySelector('.dice-1');
+        dadoDOM_0.style.display = 'block';
+        dadoDOM_1.style.display = 'block';
+        dadoDOM_0.style.opacity = 1;
+        dadoDOM_1.style.opacity = 1;
+        dadoDOM_0.src = 'Resources/dice-' + dado_0 + '.png';
+        dadoDOM_1.src = 'Resources/dice-' + dado_1 + '.png';
+        if(dado_0 !== 1 && dado_1 !== 1){
+            if(dado_0 === 6 && dado_1 === 6){
+                roundScore = 0;
+                document.querySelector('#current-' + jogadorAtivo).textContent = roundScore;
+                scores[jogadorAtivo] = 0;
+                document.getElementById('score-' + jogadorAtivo).textContent = '0';
+                proximoJogador();
+                dadoDOM_0.style.display = 'none';
+                dadoDOM_1.style.display = 'none';
+            }else{
+                roundScore += (dado_0 + dado_1);
+                document.querySelector('#current-' + jogadorAtivo).textContent = roundScore;
+            }
         }else{
             roundScore = 0;
             document.querySelector('#current-' + jogadorAtivo).textContent = roundScore;
-            dadoDOM.style.opacity = 0.5;
+            dadoDOM_0.style.opacity = 0.5;
+            dadoDOM_1.style.opacity = 0.5;
             proximoJogador();
         }
     }
@@ -56,11 +73,20 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
     if(jogoRolando){
-        document.querySelector('.dice').style.display = 'none';
+        var input = document.querySelector('.Score-Final').value;
+        var vencer;
+        if(input){
+            vencer = input; 
+        }else{
+            vencer = 100;
+        }
+        document.querySelector('.dice-0').style.display = 'none';
+        document.querySelector('.dice-1').style.display = 'none';
         scores[jogadorAtivo] += roundScore;
-        if(scores[jogadorAtivo] >= 100){
+        if(scores[jogadorAtivo] >= vencer){
             document.querySelector('#name-' + jogadorAtivo).textContent = 'Vencedor!';
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice-0').style.display = 'none';
+            document.querySelector('.dice-1').style.display = 'none';
             document.querySelector('.player-' + jogadorAtivo + '-panel').classList.toggle('winner');
             document.querySelector('.player-' + jogadorAtivo + '-panel').classList.remove('active');
             document.querySelector('#score-' + jogadorAtivo).textContent = scores[jogadorAtivo];
